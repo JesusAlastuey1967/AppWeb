194d378c3946be0e089f3b97a297915306c7e757
Jesus Alastuey <jaalastuey@gmail.com>
2022-06-06 12:44:49 +0200

    5

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('/', (req, res) => {
 console.log('Petición recibida')
 
-res.send('Hola mundo con NODEMONSABCDE¡')
+res.send('Hola mundo con NODEMONSABCDEF¡')
 })
 
 app.listen(4000, () => {
