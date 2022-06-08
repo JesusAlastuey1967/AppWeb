90887f5c9f321fb84f7353ea4f75fe08f238f9dd
Jesus Alastuey <jaalastuey@gmail.com>
2022-06-08 17:51:21 +0200

    7

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 require('dotenv').config()
 
 const express = require('express')
+const path =require('path')
 
 const app = express()
 
+app.use(express.static(path.join(__dirname,'public')))
+
 app.get('/', (req, res) => {
 console.log('Petición recibida')
-
-res.send('Hola mundo con NODEMONSABCDEFGH¡')
 })
 
 const PORT = process.env.PORT || 4000
