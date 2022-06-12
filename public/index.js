33e7ae0096371b65789f1d7f5546f632c61bc226
Jesus Alastuey <jaalastuey@gmail.com>
2022-06-12 17:06:06 +0200

    Comparacion CSR (Client Side Rendering) - SSR (Server Side Rendering) -v1

--- public/index.js
+++ public/index.js
@@ -2,13 +2,7 @@ const inputName = document.querySelector('#productName')
 const inputPrice = document.querySelector('#productPrice')
 const button = document.querySelector('button')
 const pokeApiBaseUrl ='https://pokeapi.co/api/v2/pokemon'
-
-fetch(`${pokeApiBaseUrl}/charmeleon`)
-.then((res) =>{
-    return res.json()
-})
-.then((pokemon) => {
-    console.log({pokemon})
+const createPokemon = (pokemon) => {
     const html = `
     <h3>${pokemon.name}</h3>
     <img src=${pokemon.sprites.front_default} alt="Esta es una imagen del pokemon:${pokemon.name}">
@@ -18,8 +12,23 @@ fetch(`${pokeApiBaseUrl}/charmeleon`)
     div.classList.add('poke-card')
     div.innerHTML = html
     document.querySelector('body').appendChild(div)
+}
+
+fetch(`${pokeApiBaseUrl}/charmeleon`)
+.then((res) =>{
+    return res.json()
 })
-.catch()
+.then((pokemon) => {
+    createPokemon(pokemon)
+})
+
+fetch(`${pokeApiBaseUrl}/blastoise`)
+.then((res) =>{
+    return res.json()
+})
+.then((pokemon) => {
+    createPokemon(pokemon)
+})
 
 button.addEventListener('click', (e) =>
 {
