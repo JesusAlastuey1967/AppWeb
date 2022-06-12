c7f4203f13a55ce26adb58edf3c3a54d40d5c241
Jesus Alastuey <jaalastuey@gmail.com>
2022-06-12 16:18:29 +0200

    Comparacion CSR (Client Side Rendering) - SSR (Server Side Rendering)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios')
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
@@ -15,7 +16,6 @@ console.log(`Servidor escuchando en el puerto ${PORT}`)
 })
 console.log('Conexión Exitosa')})
 .catch((err) => console.log(err))
-
 const productSchema = mongoose.Schema({
     name: {type:String, required:true},
     price: Number,
@@ -24,16 +24,12 @@ const productSchema = mongoose.Schema({
 )
 
 const Product = mongoose.model('Product', productSchema)
-
 app.use(express.json())
-
 app.post('/api/v1/products', (req, res, next) => {
-
     const newProduct = new Product({
         name: req.body.name,
         price: req.body.price,
     })
-
     newProduct.save()
     .then((result) => {
         res.status(201).json({Ok: true})
@@ -41,7 +37,56 @@ app.post('/api/v1/products', (req, res, next) => {
     .catch((err) => console.log(err))
 })
 
+app.get('/',(req, res, next) =>{
+    const pokeApiBaseUrl ='https://pokeapi.co/api/v2/pokemon'
+    axios(`${pokeApiBaseUrl}/charmander`)
+    .then((axiosResponse) => {
+    const pokemon = axiosResponse.data
+    console.log({pokemon})
+const html =`
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Productos</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="index.js" defer></script>
+</head>
+<body>
+<h1>Nuestra tremenda App de Productos</h1>
+<div class="form-container">
+    <input type="text" id="productName" placeholder="Nombre del Producto">
+    <input type="number" id="productPrice" placeholder="Precio del Producto">
+    <button>Crear Producto</button>
+    <h2>Listado de Productos</h2>
+</div> 
+<div class='poke-card'> 
+    <h3>${pokemon.name}</h3>
+    <img src=${pokemon.sprites.front_default} alt="Esta es una imagen del pokemon:${pokemon.name}">
+    <span>${pokemon.id}</span> 
+</div>
+</body>
+`
+res.send(html)
+})
+
+// .then((pokemon) => {
+//     console.log({pokemon})
+//     const html = `
+//     <h3>${pokemon.name}</h3>
+//     <img src=${pokemon.sprites.front_default} alt="Esta es una imagen del pokemon:${pokemon.name}">
+//     <span>${pokemon.id}</span>    
+//     `
+//     const div = document.createElement('div')
+//     div.classList.add('poke-card')
+//     div.innerHTML = html
+
+//     document.querySelector('body').appendChild(div)
+// })
+// .catch()
+
+})
+
 app.use(express.static(path.join(__dirname,'public')))
 
 const PORT = process.env.PORT || 4000
-
--- public/index.js
+++ public/index.js
@@ -1,13 +1,30 @@
 const inputName = document.querySelector('#productName')
 const inputPrice = document.querySelector('#productPrice')
-
 const button = document.querySelector('button')
+const pokeApiBaseUrl ='https://pokeapi.co/api/v2/pokemon'
+
+fetch(`${pokeApiBaseUrl}/charmeleon`)
+.then((res) =>{
+    return res.json()
+})
+.then((pokemon) => {
+    console.log({pokemon})
+    const html = `
+    <h3>${pokemon.name}</h3>
+    <img src=${pokemon.sprites.front_default} alt="Esta es una imagen del pokemon:${pokemon.name}">
+    <span>${pokemon.id}</span>    
+    `
+    const div = document.createElement('div')
+    div.classList.add('poke-card')
+    div.innerHTML = html
+    document.querySelector('body').appendChild(div)
+})
+.catch()
 
 button.addEventListener('click', (e) =>
 {
     const name = inputName.value 
     const price = inputPrice.value
-
     fetch('api/v1/products',{
         method: 'POST',
         headers:{
@@ -19,7 +36,5 @@ button.addEventListener('click', (e) =>
         })
     })
 })
-
-
 
 console.log({inputName, inputPrice, button})
